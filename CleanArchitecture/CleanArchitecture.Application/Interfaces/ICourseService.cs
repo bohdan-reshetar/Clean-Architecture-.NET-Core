89040cb94f35ca3a6fe794125b8874f4c38749bc
Bohdannn <bohdanreshetar@gmail.com>
2020-06-21 20:20:27 +0200

    Add Service and Repository Itnerfaces, CourseViewModel

---
+++ CleanArchitecture/CleanArchitecture.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitecture.Application.ViewModels;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Application.Interfaces
+{
+    interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
