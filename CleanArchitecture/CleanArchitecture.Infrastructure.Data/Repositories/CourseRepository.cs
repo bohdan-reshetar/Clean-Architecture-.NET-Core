4beddcab2518b198327dfe5a682ca33b58c7be57
Bohdannn <bohdanreshetar@gmail.com>
2020-06-21 22:49:41 +0200

    Add implementations of Service and Repository GetCourses Methods

---
+++ CleanArchitecture/CleanArchitecture.Infrastructure.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _universityDbContext;
+
+        public CourseRepository(UniversityDbContext universityDbContext)
+        {
+            _universityDbContext = universityDbContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _universityDbContext.Courses;
+        }
+    }
+}
