7df7bd0d6cc3c1cc35d279b19778fc759a5bccb5
Bohdannn <bohdanreshetar@gmail.com>
2020-06-22 02:14:07 +0200

    Commit changes for Presentation Course Controller, Views, CSS

---
+++ CleanArchitecture/CleanArchitecture.MVC/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var courseViewModel = _courseService.GetCourses();
+            return View(courseViewModel);
+        }
+    }
+}
